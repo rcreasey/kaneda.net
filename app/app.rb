70612e62958e565a5e58f35305d3660b24b9e8c8
Ryan C. Creasey <ryan@kaneda.net>
2011-11-27 17:28:26 -0800

    added in-line javascript and 404 error pages.

--- app/app.rb
+++ app/app.rb
@@ -5,7 +5,10 @@ class KanedaNet < Padrino::Application
   register Padrino::Helpers
 
   enable :sessions
-
+  disable :flash
+  
+  layout :application
+  
   ##
   # Caching support
   #
@@ -20,36 +23,12 @@ class KanedaNet < Padrino::Application
   #   set :cache, Padrino::Cache::Store::Memory.new(50)
   #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
   #
-
-  ##
-  # Application configuration options
-  #
-  # set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
-  # set :dump_errors, true        # Exception backtraces are written to STDERR (default for production/development)
-  # set :show_exceptions, true    # Shows a stack trace in browser (default for development)
-  # set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
-  # set :public_folder, "foo/bar" # Location for static assets (default root/public)
-  # set :reload, false            # Reload application files (default in development)
-  # set :default_builder, "foo"   # Set a custom form builder (default 'StandardFormBuilder')
-  # set :locale_path, "bar"       # Set path for I18n translations (default your_app/locales)
-  # disable :sessions             # Disabled sessions by default (enable if needed)
-  # disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
-  # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
-  #
-
+  
+  error 404, :provides => [:html] do
+    render 'errors/404'
+  end
+  
   get :index, :provides => [:html] do
     render 'prompt/index'
   end
-
-  ##
-  # You can manage errors like:
-  #
-  #   error 404 do
-  #     render 'errors/404'
-  #   end
-  #
-  #   error 505 do
-  #     render 'errors/505'
-  #   end
-  #
 end
