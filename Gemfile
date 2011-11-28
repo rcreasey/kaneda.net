634195ed6f3b6d96dbcfe64a0abd107661d0ad64
Ryan C. Creasey <ryan@kaneda.net>
2011-11-27 18:48:10 -0800

    adding memcache stuffs.

--- Gemfile
+++ Gemfile
@@ -1,14 +1,15 @@
 source :rubygems
 
-gem 'activerecord', :require => "active_record"
+gem 'activerecord', :require => 'active_record'
+gem 'dalli'
 gem 'haml'
-gem 'mocha', :group => "test"
+gem 'mocha', :group => 'test'
 gem 'newrelic_rpm'
 gem 'redcarpet'
-gem 'rack-test', :require => "rack/test", :group => "test"
+gem 'rack-test', :require => 'rack/test', :group => 'test'
 gem 'rake'
 gem 'sass'
-gem 'shoulda', :group => "test"
+gem 'shoulda', :group => 'test'
 gem 'sinatra-flash', :require => 'sinatra/flash'
 gem 'sqlite3'
 gem 'thin'
--- app/app.rb
+++ app/app.rb
@@ -1,32 +1,22 @@
 class KanedaNet < Padrino::Application
   register ScssInitializer
-  register Padrino::Rendering
-  register Padrino::Mailer
+  register Padrino::Cache
   register Padrino::Helpers
-
+  register Padrino::Mailer
+  register Padrino::Rendering
+  
   enable :sessions
-  disable :flash
+  enable :caching
+  
+  set :cache, Padrino::Cache::Store::Memcache.new( ::Dalli::Client.new )
   
   layout :application
   
   configure :production do
+    ENV['APP_ROOT'] ||= File.dirname(__FILE__)
+    $:.unshift "#{ENV['APP_ROOT']}/vendor/plugins/newrelic_rpm/lib"
     require 'newrelic_rpm'
   end
-  
-  ##
-  # Caching support
-  #
-  # register Padrino::Cache
-  # enable :caching
-  #
-  # You can customize caching store engines:
-  #
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0))
-  #   set :cache, Padrino::Cache::Store::Memory.new(50)
-  #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
-  #
   
   error 404, :provides => [:html] do
     render 'errors/404'
