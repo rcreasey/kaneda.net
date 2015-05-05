a0659211f7042b1e0211a75a391c044aa4d07c0c
Ryan C. Creasey <ryan@kaneda.net>
2015-05-04 23:08:15 -0700

    got index hbs and stylesheets working.

---
+++ config/environment.js
@@ -0,0 +1,56 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'kaneda.net',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    contentSecurityPolicy: {
+       'default-src': "'none'",
+       'script-src': "'self'",
+       'font-src': "'self' data: fonts.gstatic.com",
+       'connect-src': "'self'",
+       'img-src': "'self'",
+       'style-src': "'self' fonts.googleapis.com",
+       'media-src': "'self'"
+    },
+    
+    APP: {
+
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
