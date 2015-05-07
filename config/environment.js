7d966c18f9744f5a732e268e87823c6676d35d7d
Ryan C. Creasey <ryan@kaneda.net>
2015-05-06 23:23:52 -0700

    typewriter done.

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,7 @@ module.exports = function(environment) {
 
     contentSecurityPolicy: {
        'default-src': "'none'",
-       'script-src': "'self'",
+       'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
        'font-src': "'self' data: fonts.gstatic.com",
        'connect-src': "'self'",
        'img-src': "'self'",
